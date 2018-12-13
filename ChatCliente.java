bfcb130277f3d2daa1c446405cd989d8454c77fb
RonaldMedeiiros <ronaldmedeiiros@gmail.com>
2018-12-13 17:14:18 -0300

    Add files via upload

---
+++ ChatCliente.java
@@ -0,0 +1,12 @@
+package chatcliente;
+
+
+public class ChatCliente {
+
+    
+    public static void main(String[] args) {
+        Identificador id = new Identificador();
+        id.setVisible(true);
+    }
+    
+}
